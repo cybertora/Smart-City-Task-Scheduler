1e826cf311c080d562598fadb8fae90b5b7a1b38
rassul <rtokatov@gmail.com>
2025-11-02 18:39:34 +0500

    test util

--- src/test/java/graph/TestUtils.java
+++ src/test/java/graph/TestUtils.java
@@ -1,4 +1,10 @@
 package graph;
 
+import java.io.IOException;
+
 public class TestUtils {
+    public static Graph loadGraph(String filename) throws Exception {
+        String path = "data/" + filename;
+        return GraphReader.fromJson(path);
+    }
 }
