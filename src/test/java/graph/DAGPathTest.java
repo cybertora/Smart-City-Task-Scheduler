f9fdd8de7a1067e3b85e2f0f8f1ad62674349d33
rassul <rtokatov@gmail.com>
2025-11-02 19:59:49 +0500

    test debug

--- src/test/java/graph/DAGPathTest.java
+++ src/test/java/graph/DAGPathTest.java
@@ -13,18 +13,23 @@ public class DAGPathTest {
         Graph g = TestUtils.loadGraph("tasks_02.json");
         KahnTopologicalSort topo = new KahnTopologicalSort(g);
         List<Integer> order = topo.sort();
-        assertNotNull(order);
+        assertNotNull(order, "Граф должен быть DAG");
 
         DAGShortestLongestPath path = new DAGShortestLongestPath(g, order);
         path.computeFromSource(g.source);
 
         long[] dist = path.getShortestDistances();
-        assertEquals(0, dist[0]);
-        assertEquals(10, dist[3]); // 0→2→3 = 3+2=5? Проверим граф
 
-        long critical = path.getCriticalPathLength();
-        assertEquals(16, critical); // 0→1→3→4→5 = 5+6+4+1 = 16
+        assertArrayEquals(new long[]{
+                0,
+                5,
+                3,
+                5,
+                9,
+                10
+        }, dist);
 
+        assertEquals(16, path.getCriticalPathLength());
         List<Integer> critPath = path.getCriticalPath();
         assertEquals(List.of(0, 1, 3, 4, 5), critPath);
     }
