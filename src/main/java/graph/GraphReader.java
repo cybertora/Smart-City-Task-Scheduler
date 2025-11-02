de0e3126f46446c4de44b3c1088739bc525e97e2
rassul <rtokatov@gmail.com>
2025-11-02 17:44:31 +0500

    graph reader class

--- src/main/java/graph/GraphReader.java
+++ src/main/java/graph/GraphReader.java
@@ -1,4 +1,24 @@
 package graph;
 
+import com.google.gson.*;
+import java.io.FileReader;
+
 public class GraphReader {
+    public static Graph fromJson(String path) throws Exception {
+        JsonObject obj = JsonParser.parseReader(new FileReader(path)).getAsJsonObject();
+        int n = obj.get("n").getAsInt();
+        int source = obj.get("source").getAsInt();
+        boolean directed = obj.get("directed").getAsBoolean();
+
+        Graph g = new Graph(n, source, directed);
+        JsonArray edges = obj.getAsJsonArray("edges");
+        for (JsonElement e : edges) {
+            JsonObject edge = e.getAsJsonObject();
+            int u = edge.get("u").getAsInt();
+            int v = edge.get("v").getAsInt();
+            int w = edge.get("w").getAsInt();
+            g.addEdge(u, v, w);
+        }
+        return g;
+    }
 }
